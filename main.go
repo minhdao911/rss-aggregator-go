1ca9f2f460d03774505260fac071f8e0a0455f9e
Minh Dao <minh@flowrite.com>
2023-09-12 14:33:13 +0300

    boilerplate handlers and json respond ready

--- main.go
+++ main.go
@@ -29,6 +29,13 @@ func main() {
 		MaxAge: 300,
 	}))
 
+	v1Router := chi.NewRouter()
+
+	v1Router.Get("/healthz", handlerReadiness)
+	v1Router.Get("/err", handlerErr)
+
+	router.Mount("/v1", v1Router)
+
 	server := &http.Server{
 		Handler: router,
 		Addr: ":" + portString,
